f02e855224fa4c9594d11cf7827a7b2a3705f0a1
jaya <jayaseyyadri@gmail.com>
2020-12-30 18:01:24 -0600

    working on 101 exercises

--- src/Java101Exercises.java
+++ src/Java101Exercises.java
@@ -186,7 +186,6 @@ public class Java101Exercises {
         public static int calcRemainder010(int a, int b) {
             // TODO: Write code here
             return a % b;
-            return 0;
         }
 
         /**
@@ -382,5 +381,86 @@ public class Java101Exercises {
         public static int convertRomanNumeral072(String romanNumeral) {
             return 0;
         }
-    }
+
+//
+//
+//
+//
+
+/*Implement a function , that takes an integer and returns the sum of the factorials of each digit of n.
+    For example, if number is equal to 145, we want the sum of 1! + 4! + 5!
+
+    int factorialDigits(int number)
+
+    Example: factorialDigits(321)
+
+    Result: 9
+
+    Fibonacci sequence
+    Implement a function, which takes an integer n and returns a number, which is formed by concatenating the first n Fibonacci numbers.
+
+    For example, if n = 3, the result must be 112.
+
+    int fibonacciSequence(int number)
+
+    Example: fibonacciSequence(5)
+
+    Result: 11235
+
+    Is a given number palindrome
+    Implement a function, which takes an integer number and checks if the given number is palindrome.
+
+    For example, the integer 121 is palindrome, but 12340321 - not!
+
+    boolean isPalindrome(int number)
+
+    Example: isPalindrome(12321)
+
+    Result: true
+
+    Reverse the odd words in string
+    Implement a function, which takes a string and returns a string object containing reversed only the words of odd index.
+
+            String reverseOddWords(String sentence)
+
+    Example: reverseOddWords(This is the first lecture for Programming 101 with Java)
+
+    Result: This si the tsrif lecture rof Programming 101 with avaJ
+
+    Check the sum of number and reversedNumber is palindrome
+    Write a function, which takes a number and returns boolean if the sum of the number and it's reversed number is palindrome.
+
+    boolean isSumPalindrome(int input)
+
+    Example: isSumPalindrome(123)
+
+    Result: true
+
+            123 + 321 = 444
+
+    Find the sum of the divisors in array from numberA to numberB
+    Implement a function, which takes two integer numbers and returns the sum of the divisors from each number in the sequence from numberA to numberB
+
+    int sumOfDivisors(int numberA, numberB)
+
+    Example: sumOfDivisors(4, 7)
+
+    Divisors of 4: 4, 2, 1 Divisors of 5: 5, 1 Divisors of 6: 6, 3, 2, 1 Divisors of 7: 7, 1
+
+    Result: 33
+
+    Check the number of vowels, consonants and digits in string
+    Implement a function, which takes a string sentence and returns the number of the vowels, consonants and numbers in the given string object.
+
+            String [] vowelsConsonantsDigits(String sentence)
+
+    Example: vowelsConsonantsDigits("It is 18th of Oct 2016")
+
+    Result: ["Vowels: 4", "Consonants: 7", "Digits: 6"]
+
+*/
+
+
+
+}
 
