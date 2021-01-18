cc48d633ec8c4438034b0880f0c224e0687cfcc8
jaya <jayaseyyadri@gmail.com>
2021-01-17 23:04:53 -0600

    coding bat

--- src/digitsOfNumber.java
+++ src/digitsOfNumber.java
@@ -2,27 +2,28 @@ public class digitsOfNumber {
 
     public static void main(String[] args) {
 
-       int count=0;
+        int count = 0;
         int number1 = 2078880;
         while (number1 != 0) {
-            number1=number1 / 10;
+            number1 = number1 / 10;
             count++;
             System.out.println(number1);
             System.out.println(count);
-
-
-     /**   ¨Given a string of odd length, return the string length 3 from its middle, so "Candy" yields "and". The string length will be at least 3.
-//
-//
-//middleThree("Candy") → "and"
-//middleThree("and") → "and"
-//middleThree("solving") → "lvi"
-
-            public String middleThree(String str) {
-                String a = "";
-                int middle =str.length()/2-1;
-                return str.substring(middle,middle+3);
-
-            }
+        }
     }
+  /**Given a string of odd length, return the string length 3 from its middle, so "Candy" yields "and". The string length will be at least 3
+
+
+    middleThree("Candy") → "and"
+    middleThree("and") → "and"
+    middleThree("solving") → "lvi"
+
+            **/
+
+  public String middleThree(String str) {
+      String a = "";
+      int middle =str.length()/2-1;
+      return str.substring(middle,middle+3);
+
+  }
 }
