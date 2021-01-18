cdea6e83a40312d7f3d9d08cd32d1ccfa2393a3f
jaya <jayaseyyadri@gmail.com>
2021-01-17 22:56:41 -0600

    coding bat number

--- src/digitsOfNumber.java
+++ src/digitsOfNumber.java
@@ -9,6 +9,20 @@ public class digitsOfNumber {
             count++;
             System.out.println(number1);
             System.out.println(count);
-        }
+
+
+     /**   ¨Given a string of odd length, return the string length 3 from its middle, so "Candy" yields "and". The string length will be at least 3.
+//
+//
+//middleThree("Candy") → "and"
+//middleThree("and") → "and"
+//middleThree("solving") → "lvi"
+
+            public String middleThree(String str) {
+                String a = "";
+                int middle =str.length()/2-1;
+                return str.substring(middle,middle+3);
+
+            }
     }
 }
