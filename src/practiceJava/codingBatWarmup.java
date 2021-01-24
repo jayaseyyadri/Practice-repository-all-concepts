c6b34f1443e055657ea07e4484268f019e3bf2b3
jaya <jayaseyyadri@gmail.com>
2021-01-23 23:33:30 -0600

    codingBatWarmup2

--- src/practiceJava/codingBatWarmup.java
+++ src/practiceJava/codingBatWarmup.java
@@ -4,6 +4,12 @@ public class codingBatWarmup {
 
 
 //    Given a string and a non-negative int n, return a larger string that is n copies of the original string.
-
+public String stringTimes(String str, int n) {
+    String newString="";
+    for(int i=0;i<n;i++){
+        newString+=str ;
+    }
+    return newString;
+}
 
 }
