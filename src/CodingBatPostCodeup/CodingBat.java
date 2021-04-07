f38dfc156164115ff6bac1a321e36f4f605e3b16
jaya <jayaseyyadri@gmail.com>
2021-04-07 00:20:26 -0500

    codingBat problems

--- src/CodingBatPostCodeup/CodingBat.java
+++ src/CodingBatPostCodeup/CodingBat.java
@@ -111,6 +111,16 @@ public static boolean shareDigit(int a, int b) {
             return "error";
 
     }
+
+   /** The number 6 is a truly great number. Given two int values, a and b, return true if either one is 6. Or if their sum or difference is 6. Note: the function Math.abs(num) computes the absolute value of a number.*/
+   public static boolean love6(int a, int b) {
+       if (a==6 || b==6 || a+b==6||a-b==6 || b-a ==6){
+           return true;
+       }
+       return false;
+   }
+
+
     public static void main(String[] args) {
         System.out.println(CodingBat.blueTicket(9,1,0));
         System.out.println(CodingBat.shareDigit(11,22));
@@ -119,6 +129,7 @@ public static boolean shareDigit(int a, int b) {
         System.out.println(CodingBat.maxMod5(20,10));
         System.out.println(CodingBat.sortaSum(2,5));
         System.out.println(CodingBat.alarmClock(6,false));
+        System.out.println(CodingBat.love6(5,9));
 
  }
 
