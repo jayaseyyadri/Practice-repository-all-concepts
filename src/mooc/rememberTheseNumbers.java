32d79574b0cb45cef64e805eed34aaea4cb0e0f3
jaya <jayaseyyadri@gmail.com>
2021-05-20 23:32:46 -0700

    mooc

--- src/mooc/rememberTheseNumbers.java
+++ src/mooc/rememberTheseNumbers.java
@@ -1,4 +1,12 @@
-package mooc;
-/**program so that after reading the numbers, it prints all the numbers received from the user. The number used to indicate stopping should not be printed.*/
-public class rememberTheseNumbers {
-}
+//package mooc;
+//
+//import java.util.Scanner;
+//
+///**program so that after reading the numbers, it prints all the numbers received from the user. The number used to indicate stopping should not be printed.*/
+//public class rememberTheseNumbers {
+//    Scanner scanner = new Scanner(System.in);
+//    int userInput = Integer.valueOf(scanner.nextLine());
+//do{
+//        userInput = Integer.valueOf(scanner.nextLine());
+//    }while(userInput != -1);
+//}
